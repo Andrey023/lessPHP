4dbe15ee2279de4bbbd81d79ceeb97ed8642502f
Philippov A <andrey.philip_18@mail.ru>
2021-07-04 14:19:51 +0300

    make lesson 9 task_9.php addContent.php

---
+++ addContent.php
@@ -0,0 +1,12 @@
+<?php
+	$user = 'root';
+    $pass = 'philip21';
+    $dbname = 'lessPHP';
+    $host = 'localhost';
+    $dsn = 'mysql:host='.$host.';dbname='.$dbname.';';
+    $linkDB = new PDO($dsn, $user, $pass);
+	$content = htmlspecialchars($_POST['textUser']);
+	$STMT = $linkDB->prepare("insert into addedText (userAddedText) VALUES ('".$content."')");
+	// var_dump($STMT);
+	$STMT->execute();
+?>

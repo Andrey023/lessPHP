faa63b8270f88e14c62f2b2cb289898f15a76b17
Philippov A <andrey.philip_18@mail.ru>
2021-07-07 00:36:44 +0300

    make lesson task10

--- addContent.php
+++ addContent.php
@@ -1,13 +1,28 @@
 <?php
+	session_start();
 	$user = 'root';
     $pass = 'philip21';
     $dbname = 'lessPHP';
     $host = 'localhost';
     $dsn = 'mysql:host='.$host.';dbname='.$dbname.';';
     $linkDB = new PDO($dsn, $user, $pass);
-	$content = htmlspecialchars($_POST['textUser']);
-	$STMT = $linkDB->prepare("insert into addedText (userAddedText) VALUES ('".$content."')");
-	// var_dump($STMT);
+	$content = $_POST['usertext'];
+	$getContent = "SELECT * FROM addedText WHERE userAddedText = '".$content."'";
+	// $STMT = $linkDB->prepare("insert into addedText (userAddedText) VALUES ('".$content."')");
+	$STMT = $linkDB->prepare($getContent);
 	$STMT->execute();
+	$getData = $STMT -> fetch(PDO::FETCH_ASSOC);
+	if(!empty($getData)){
+		$_SESSION['checkedDB'] = "danger";
+		$_SESSION['msg'] = "Текст существует! Повторите ввод!";
+	}
+	else{
+		$setContent = "insert into addedText (userAddedText) VALUES ('".$content."')";
+		$STMT = $linkDB->prepare($setContent);
+		$STMT -> execute();
+		$_SESSION['checkedDB'] = "success";
+		$_SESSION['msg'] = "Текст принят и добавлен в базу!";
+	}
+	header('location: task_10.php');
 	$linkDB = null;
 ?>
